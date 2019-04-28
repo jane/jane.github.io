857dcaf3bb45704fbe379db9a39dab1e98b1ce13
zacanger <zac@zacanger.com>
2019-04-27 18:28:23 -0600

    Update deps

---
+++ opensource/precache-manifest.814661829a59993b7416d669bf028c4f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091284baa862127d7a27f6947bf7f0b5",
+    "url": "/opensource/index.html"
+  },
+  {
+    "revision": "d8013d288cc0c5e00d25",
+    "url": "/opensource/static/js/2.24378331.chunk.js"
+  },
+  {
+    "revision": "390f7e1f76c16e30173b",
+    "url": "/opensource/static/js/main.5c48d086.chunk.js"
+  },
+  {
+    "revision": "e382209c401664bc2852",
+    "url": "/opensource/static/js/runtime~main.c717c4b7.js"
+  }
+]);
--- opensource/service-worker.js
+++ opensource/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/opensource/precache-manifest.7d6f7a47a88e0606c4fba5d8788a84f4.js"
+  "/opensource/precache-manifest.814661829a59993b7416d669bf028c4f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/opensource/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/opensource/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
