2934437dcb3379b5a6e758a15a68b203769e2259
zacanger <zac@zacanger.com>
2019-10-11 13:41:20 -0600

    build

---
+++ opensource/precache-manifest.7161186c713b48a6c34bb8d5b61333ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247b9b137a4fc5a4c2ae0b44a65b9578",
+    "url": "/opensource/index.html"
+  },
+  {
+    "revision": "69f230b2d2e8414bfc3d",
+    "url": "/opensource/static/js/2.8dd8a5d8.chunk.js"
+  },
+  {
+    "revision": "a9c53010b19113770ce1",
+    "url": "/opensource/static/js/main.79604928.chunk.js"
+  },
+  {
+    "revision": "2fe0f29d23af7dd87010",
+    "url": "/opensource/static/js/runtime-main.ca45a5f4.js"
+  }
+]);
--- opensource/service-worker.js
+++ opensource/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/opensource/precache-manifest.b906ace77c8acf05d87c493921a1ab93.js"
+  "/opensource/precache-manifest.7161186c713b48a6c34bb8d5b61333ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/opensource/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
